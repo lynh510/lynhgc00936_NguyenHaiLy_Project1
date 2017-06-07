d91014909de21df634f8fee88f72b257a9b05ef7
LY <lynhgc00936@fpt.edu.vn>
2017-06-07 18:58:40 +0700

    fix

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,12 +6,10 @@
 package app;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
